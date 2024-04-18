0acccef82c04604b234bfce5d7c1d806ceeedc11
Manmit Pal <manmitpal348@gmail.com>
2024-04-18 19:55:31 +0530

    Igniting our app

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", {id:"parent"},[
     React.createElement("child1",{id:"child1"},
                                     [React.createElement("h1", {}, "Im an h1 tag"),React.createElement("h2", {}, "Im an h2 tag")]),React.createElement("child2",{id:"child2"},
