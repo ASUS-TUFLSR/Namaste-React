28a0fee48d54d26cb72c15c8bc25fe6cc5af6cac
Manmit Pal <manmitpal348@gmail.com>
2024-04-26 21:03:23 +0530

    added filter

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,106 +2,29 @@ import React from 'react'
 import RestaurantCard from './RestaurantCard'
 import { resList } from '../utils/mockData'
 import {Row, Col} from 'react-bootstrap'
+import { useState } from 'react'
 
 
   
 const Body = () => {
 
-    let listOfRestaurants = [
-        {
-            "info": {
-              "id": "323526",
-              "name": "Chinese Wok",
-              "cloudinaryImageId": "e0839ff574213e6f35b3899ebf1fc597",
-              "locality": "Chinchwad",
-              "areaName": "Chinchwad",
-              "costForTwo": "₹300 for two",
-              "cuisines": [
-                "Chinese",
-                "Asian",
-                "Tibetan",
-                "Desserts"
-              ],
-              "avgRating": 3.9,
-              "parentId": "61955",
-              "avgRatingString": "3.9",
-              "totalRatingsString": "5K+",
-              
-            "cta": {
-              "link": "https://www.swiggy.com/restaurants/chinese-wok-chinchwad-pune-323526",
-              "type": "WEBLINK"
-            }
-        }
-          },
-          {
-            "info": {
-              "id": "611808",
-              "name": "UBQ by Barbeque Nation",
-              "cloudinaryImageId": "noc7ieivirqxtpujhsgl",
-              "locality": "1st Phase",
-              "areaName": "Hinjawadi",
-              "costForTwo": "₹300 for two",
-              "cuisines": [
-                "North Indian",
-                "Barbecue",
-                "Biryani",
-                "Kebabs",
-                "Mughlai",
-                "Desserts"
-              ],
-              "avgRating": 3.9,
-              "parentId": "10804",
-              "avgRatingString": "3.9",
-              "totalRatingsString": "100+",
-              
-            "cta": {
-              "link": "https://www.swiggy.com/restaurants/ubq-by-barbeque-nation-1st-phase-hinjawadi-pune-611808",
-              "type": "WEBLINK"
-            }
-        }
-          },
-          {
-            "info": {
-              "id": "611807",
-              "name": "Barbeque Nation",
-              "cloudinaryImageId": "iydxtay1mnne2ktw7txe",
-              "locality": "Global Highstreet Mall",
-              "areaName": "Hinjawadi",
-              "costForTwo": "₹600 for two",
-              "cuisines": [
-                "North Indian",
-                "Barbecue",
-                "Biryani",
-                "Kebabs",
-                "Mughlai",
-                "Desserts"
-              ],
-              "avgRating": 3.6,
-              "parentId": "2438",
-              "avgRatingString": "3.6",
-              "totalRatingsString": "100+",
-              
-            "cta": {
-              "link": "https://www.swiggy.com/restaurants/barbeque-nation-global-highstreet-mall-hinjawadi-pune-611807",
-              "type": "WEBLINK"
-            }
-          }
-        }
-    ];
+    const [listOfRestaurants, setListOfRestaurants] = useState(resList);
 
+  
     return (
         <>
         <Col><button className='btn btn-dark' onClick={() => {
-            listOfRestaurants = listOfRestaurants.filter((res) => res.info.avgRating > 3.7 );
-            console.log(listOfRestaurants)
+          const filteredList = listOfRestaurants.filter((res) => res.info.avgRating > 3.9 );
+           setListOfRestaurants(filteredList);
+           console.log(filteredList)
         }} >
-            Search <i class="fas fa-search"></i></button>
+          Filter <i class="fas fa-search"></i></button>
              
            </Col>
          <Row>
             
     {listOfRestaurants.map((restaurant) => (
-         <Col key={restaurant.info._id} sm={12} md={6} lg={4} xl={3} > 
+         <Col key={restaurant.info.id} sm={12} md={6} lg={4} xl={3} > 
         <RestaurantCard resData={restaurant}/>
          </Col>
     ))}
